b98108fa05101d3ed695dd9486f8b7745b5e1498
mayowasamuel <87167237+Mayowasamuel51@users.noreply.github.com>
2023-05-20 16:38:25 +0100

    Add files via upload

---
+++ sat.js
@@ -0,0 +1,69 @@
+const database = ['may', 'come', 'well', 'bisi'];
+
+
+// const checkName = prompt('what is  your name  ')
+
+// if (database.includes(checkName)) {
+//     console.log('Name is  found')
+//     alert('Name is found')
+// }else {
+//     console.log('Name is not Found')
+//     alert('Name not  found')
+//     alert('Name   NOt ound')
+// }
+// '2'
+// 2 // this want we need
+
+// const amount =  parseInt( prompt('Enter Amount   >>>>'))
+
+// if (amount ===   2) {
+//     console.log('the amount u enter is equal to 2')
+// } else if (amount >= 6) {
+//     console.log('the amount u entered  is bigger than  6 ')
+// } else if (amount < 1) {
+//     console.log('the amount u entered  is less than 1')
+// } else {
+//     console.log('OOOH SORRY I DONT UNDERSTAND YOUT INPUT')
+// }
+
+
+/// if and prompt
+// - , +, *, %, /
+// const sign =   prompt('Enter your sign');
+// const num_1 = parseInt(prompt('Enter the first number'))
+// const num_2 = parseInt(prompt('Enter the   second number'))
+
+
+// if (sign === '+') {
+//     console.log(num_1 + num_2)
+// }
+
+
+// ..................
+// if (Condition) {
+//     print out your  Statment
+// } else {
+//     statment
+// }
+
+
+const color = 'blue'
+switch (color) {
+    case 'yello':
+        console.log('this is correct');
+        break;
+    case 'blue':
+        console.log('this color is blue ')
+        break;
+    default:
+        console.log('this is not correct')
+}
+
+
+if (color === 'yellow') {
+    console.log('this is correct');
+} else if (color === 'blue') {
+    console.log('this color is blue ')
+} else {
+    console.log('this is not correct')   
+}
